200a4a83c50b610cd5f28569c235414352627f6e
nasua <acollazocacho@gmail.com>
2023-12-29 12:03:47 +0100

    ejercicio

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int numero=407;
+        System.out.println(isAmstrong(numero));
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+    public static boolean isAmstrong(int numero){
+        int suma=0;
+        String number = String.valueOf(numero);
+        char[] digitos = number.toCharArray();
+        for(int i=0;i<digitos.length;i++){
+            System.out.println(digitos[i]);
+          suma+= (int) Math.pow(Integer.parseInt(String.valueOf(digitos[i])), digitos.length);
         }
+
+        return suma==numero;
+
     }
 }
